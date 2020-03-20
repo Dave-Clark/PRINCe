2c8a8deed2e94b5aae04ec6660af68bbc5fcc7c3
Dave Clark <dclarkb@essex.ac.uk>
2020-03-20 16:38:54 +0000

    Take only 5cm depths and calc. mean per microbial core

--- total_P.R
+++ total_P.R
@@ -71,5 +71,64 @@ phosPlot <- ggplot(totalP, aes(x = label, y = conc, col = season)) +
     legend.title = element_text(size = 14),
     panel.grid = element_blank())
 
-ggsave("../graphics/total_phosphorous.pdf", phosPlot, width = 6, height = 5,
-  device = "pdf")
+totalP[, probe := as.integer(probe)]
+totalP[, depth := ifelse(probe <= 8, "5cm", "10cm")]
+
+totalP <- totalP[depth == "5cm"]
+
+totalP[treatment == "Ammonium", microbialCore := ifelse(
+  probe %in% c(1, 2, 3), 1, ifelse(
+    probe %in% c(4, 5, 6), 2, 3))]
+
+totalP[treatment == "Nitrite", microbialCore := ifelse(
+  probe %in% c(1, 2, 3), 4, ifelse(
+    probe %in% c(4, 5, 6), 5, 6))]
+
+# calculate mean P by site, season, and microb core
+microP <- totalP[, mean(conc), by = c(
+  "site", "season", "treatment", "microbialCore")]
+
+names(microP)[5] <- "total_P"
+
+p_5cm <- ggplot(microP, aes(x = site, y = total_P, col = season)) + 
+  geom_boxplot(width = 0.6) +
+  labs(x = "Site", y = expression(Total~P~(mg~L^-1))) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 16),
+    axis.title = element_text(size = 18),
+    legend.text = element_text(size = 14),
+    legend.title = element_text(size = 14),
+    panel.grid = element_blank())
+
+ggsave("../graphics/p_5cm.pdf", p_5cm, height = 5, width = 7, device = "pdf")
+
+qpcr <- fread("qPCR_raw/mergedqPCRData.csv")
+
+qpcr[, label := paste(site, core, season, sep = "_")]
+
+microP[, label := paste(site, microbialCore, season, sep = "_")]
+
+usefulCols <- c("AOA_amoA", "AOB_amoA", "Bac_16S", "Anammox_hzo", "Anammox_hzsA", "nirK_C1", "nirK_C2", "nirK_C3", "nirS_C1", "nirS_C2", "nosZ_C1", "Nitrospira_nxrB", "Thaum_ureC", "label")
+
+allDat <- merge(qpcr[, .SD, .SDcols = usefulCols], microP, by = "label")
+
+meltDat <- melt(allDat[, !c("label", "treatment", "microbialCore")],
+  id.vars = c("site", "season", "total_P"))
+
+qpcrP <- ggplot(meltDat,
+    aes(x = total_P, y = value, col = site, shape = season)) +
+  geom_point(size = 2, alpha = 0.6) +
+  facet_wrap(~ variable, scales = "free_y") +
+  labs(x = expression(Total~P~(mg~L^-1)),
+    y = expression(Gene~copies~(g^-1~dry~sediment))) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 16),
+    axis.title = element_text(size = 18),
+    legend.text = element_text(size = 14),
+    legend.title = element_text(size = 14),
+    strip.text.x = element_text(size = 14),
+    panel.grid = element_blank())
+
+ggsave("../graphics/qpcr_panel.pdf", qpcrP, height = 10, width = 13, device = "pdf")
+
+glmer.nb()
